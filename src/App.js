54bbeb0e9d1e52fd21bcae12963ef0f06281b554
Rai Pocius <pocius.raimondas@gmail.com>
2024-02-05 11:33:37 -0600

    Initial router setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Root from './components/root';
+import { RouterProvider, Route, createRoutesFromElements, createBrowserRouter } from 'react-router-dom';
 
 // Add react-router-dom imports
 
@@ -11,8 +12,7 @@ const appRouter = "REPLACE ME";
 
 function App() {
   return (
-    // replace below with a Router Provider
-    <p>REPLACE ME</p>
+    <RouterProvider router={appRouter} />
   );
 }
 
