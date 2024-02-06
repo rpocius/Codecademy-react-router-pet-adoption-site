0fd2b470a235a97b9ef648556d6127e1a3f2559d
Rai Pocius <pocius.raimondas@gmail.com>
2024-02-05 20:14:51 -0600

    Add nested routes, update links

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ import { RouterProvider, Route, createRoutesFromElements, createBrowserRouter } 
 
 // create router with JSX Route elements
 const appRouter = createBrowserRouter(createRoutesFromElements(
-  <Route path="/" element={<Root />} />
+  <Route path="/" element={<Root />}>
+    <Route index element={<HomePage />} />
+    <Route path=":type" element={<HomePage />} />
+    <Route path=":type/:id" element={ <PetDetailsPage /> } />
+  </Route>
 ));
 
 function App() {
